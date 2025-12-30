51062c63df357d71060e87a9c2153584833eee9e
vufimcev <buktor@bk.ru>
2025-12-30 05:10:37 -0800

    feat: complete getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -115,8 +115,13 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+
+  const MS_PER_DAY = 24 * 60 * 60 * 1000;
+
+  return Math.floor((end - start) / MS_PER_DAY) + 1;
 }
 
 /**
